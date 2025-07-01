92b8161afe38b104261d50fa6e394b9ddeca2821
promproexe <63090004+PromproEXE@users.noreply.github.com>
2025-07-01 20:57:37 +0700

    ADD: WEB ICON

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,44 @@ export default defineNuxtConfig({
 			htmlAttrs: {
 				lang: "th",
 			},
+			link: [
+				{
+					rel: "apple-touch-icon",
+					sizes: "180x180",
+					href: "/favicon/apple-touch-icon.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "32x32",
+					href: "/favicon/favicon-32x32.png",
+				},
+				{
+					rel: "icon",
+					type: "image/png",
+					sizes: "16x16",
+					href: "/favicon/favicon-16x16.png",
+				},
+				{
+					rel: "manifest",
+					href: "/favicon/site.webmanifest",
+				},
+				{
+					rel: "mask-icon",
+					href: "/favicon/safari-pinned-tab.svg",
+					color: "#a52241",
+				},
+			],
+			meta: [
+				{
+					name: "msapplication-TileColor",
+					content: "#a52241",
+				},
+				{
+					name: "theme-color",
+					content: "#ffffff",
+				},
+			],
 		},
 	},
 	seo: {},
