4b206d7612dd299c174b300bba1e79454483e79b
philaphat-2003 <philaphatkaewthane@gmail.com>
2025-07-03 17:27:30 +0700

    FEAT_product_type

---
+++ app/components/content/ProductGrid.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="max-w-4xl mx-auto py-5 px-4">
+    <h2 class="text-2xl font-bold mb-5 text-center">สินค้าของเรา</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+      <div
+        v-for="(item, index) in products"
+        :key="index"
+        class="bg-white rounded-lg shadow-md p-4 flex flex-col items-center"
+      >
+        <img
+          :src="item.image"
+          :alt="item.name"
+          class="w-full h-auto rounded mb-4"
+        />
+        <h3 class="text-lg font-semibold mb-2">{{ item.name }}</h3>
+        <button
+          class="bg-primary-400 text-white px-4 py-2 rounded shadow hover:bg-primary-500"
+        >
+          รายละเอียด
+        </button>
+      </div>
+    </div>
+  </section>
+
+  <!-- ข้อความข้างล่าง -->
+
+  <div div class="px-[4.6rem] py-[3rem]">
+    <h2 class="text-2xl font-bold mb-8 text-start text-primary">
+      เลือกผ้าแบบไหนให้เหมาะกับแบรนด์คุณ
+    </h2>
+    <p class="text-stone-500 text-start"><slot name="products" /></p>
+  </div>
+
+  <!-- ปุ่มๆๆๆๆๆ -->
+  <div class="flex justify-center m-5">
+    <button
+      class="relative inline-flex rounded-lg px-4 py-2 text-white bg-primary hover:bg-primary-50"
+    >
+      เนื้อผ้าและคุณสมบัติ
+      <span
+        class="absolute top-0 right-0 -mt-1 -mr-1 flex size-3 rounded-full bg-primary-200 animate-ping"
+      ></span>
+    </button>
+  </div>
+</template>
+
+<script setup>
+const products = [
+  { name: 'เสื้อยืด', image: 'product/tshirt.jpg' },
+  { name: 'เสื้อโปโล', image: 'product/polo.jpg' },
+  { name: 'เสื้อกีฬา', image: 'product/sport.png' },
+  { name: 'หมวก', image: 'product/hat.png' },
+  { name: 'หน้ากาก', image: 'product/mask.png' },
+  { name: 'กระเป๋าผ้า', image: 'product/totebag.jpg' },
+]
+</script>
+
+<style scoped></style>
---
+++ app/components/content/ProductHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="px-[4.6rem] py-[3rem]">
+    <h1
+      class="text-primary text-[4rem] leading-none font-bold text-center p-5 m-10"
+    >
+      <slot name="title" />
+    </h1>
+    <p class="text-stone-500 text-start">
+      <slot name="describetion" />
+    </p>
+    <h2
+      class="text-primary leading-none text-center p-5 m-10 text-xl font-bold"
+    >
+      สมศรีมีสินค้าพรีเมี่ยมหลากหลายชนิดไม่ว่าจะเป็น เสื้อยืด สกรีนเสื้อ หน้ากาก
+      กระเป๋าผ้า ผ้ากันเปื้อน และอื่น ๆ ให้คุณเลือกสรร
+    </h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
