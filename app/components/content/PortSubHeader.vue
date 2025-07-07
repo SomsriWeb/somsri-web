ad3d761605e6f04ba0fa62fb7e95bc0b9dd76ffd
pawataven <pawatwantrong@gmail.com>
2025-07-07 10:58:21 +0700

    Fix: port เเก้ format docs ไฟล์ที่เพิ่ม

--- app/components/content/PortSubHeader.vue
+++ app/components/content/PortSubHeader.vue
@@ -1,10 +1,7 @@
-<script setup>
-    
-</script>
+<script setup></script>
 
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 items-stretch">
-
     <div class="px-[4.6rem] py-[3rem] flex flex-col justify-center">
       <h1 class="text-start text-5xl font-bold text-primary mb-3">
         <div class="text-xl mb-1 text-primary">กลุ่มลูกค้า</div>
@@ -15,12 +12,10 @@
       </p>
     </div>
 
-<div class="relative flex items-center justify-center p-6">
-  <div class="w-full max-w-[800px]  rounded-2xl overflow-hidden shadow-md">
-    <slot name="image" />
+    <div class="relative flex items-center justify-center p-6">
+      <div class="w-full max-w-[800px] rounded-2xl overflow-hidden shadow-md">
+        <slot name="image" />
+      </div>
+    </div>
   </div>
-</div>
-  </div>
-
-  
 </template>
