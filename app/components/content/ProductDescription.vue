d90e3a46eabbe014b542071204f99f6dbb00dbb6
philaphat-2003 <philaphatkaewthane@gmail.com>
2025-07-07 11:59:25 +0700

    FEAT: PRODUCT TYPE PAGE

---
+++ app/components/content/ProductDescription.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+
+  <!-- ข้อความข้างล่าง -->
+  <div div class="px-[4.6rem] py-[3rem]">
+    <h2 class="text-2xl font-bold mb-8 text-start text-primary">
+      เลือกผ้าแบบไหนให้เหมาะกับแบรนด์คุณ
+    </h2>
+    <p class="text-stone-500 text-start"><slot name="products" /></p>
+  </div>
+
+  <!-- ปุ่มๆๆๆๆๆ -->
+  <div class="flex justify-center m-5">
+    <UButton
+      class="relative inline-flex rounded-lg px-4 py-2 text-white bg-primary hover:bg-primary-50 cursor-pointer"
+      >เนื้อผ้าและคุณสมบัติ</UButton
+    >
+  </div>
+</template>
