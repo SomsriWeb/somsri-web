67bd0ddc01f0f2cdc9d2e65aba9c6f81ec5a15d1
pawataven <pawatwantrong@gmail.com>
2025-07-10 08:51:16 +0700

    Fix: เเก้ ตาม comments ครับ

--- app/components/content/AboutHeader.vue
+++ app/components/content/AboutHeader.vue
@@ -7,19 +7,21 @@
       <slot mdc-unwrap="p" name="header" />
     </h1>
 
-<div class="flex justify-center pt-5">
-  <div class="overflow-hidden rounded-2xl shadow-md">
-    <slot mdc-unwrap="p" name="image-banner" />
-  </div>
-</div>
+    <div class="flex justify-center pt-5">
+      <div class="overflow-hidden rounded-2xl shadow-md">
+        <slot mdc-unwrap="p" name="image-banner" />
+      </div>
+    </div>
 
-    <div class="font-light text-lg pt-5">
-      <p><slot name="describetion-text" /></p>
+    <div class="font-light text-lg pt-5 text-stone-500">
+      <p>
+        <slot mdc-unwrap="p" name="description-text" />
+      </p>
     </div>
     <div
       class="flex justify-center text-stone-500 font-light text-left max-w-4xl w-full px-4"
     >
-      <slot name="describetion-list" />
+      <slot mdc-unwrap="p" name="description-list" />
     </div>
 
     <div>
@@ -27,7 +29,7 @@
         <slot mdc-unwrap="p" name="slogan-text" />
       </h1>
       <p class="text-center text-lg font-bold text-primary">
-        <slot mdc-unwrap="p" name="describetion-slogan" />
+        <slot mdc-unwrap="p" name="description-slogan" />
       </p>
     </div>
 
@@ -38,7 +40,7 @@
       <div
         class="flex justify-start font-light text-stone-500 max-w-4xl w-full px-4"
       >
-        <slot name="strategy-list" />
+        <slot mdc-unwrap="p" name="strategy-list" />
       </div>
     </div>
   </div>
