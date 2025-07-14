d012c5abb76f0d4b9333326f3da3f86926e096cd
philaphat-2003 <philaphatkaewthane@gmail.com>
2025-07-14 14:29:13 +0700

    แก้ไขUI

--- app/components/content/PoloFabric.vue
+++ app/components/content/PoloFabric.vue
@@ -8,7 +8,7 @@ const { data: fabrics } = await useAsyncData("fabrics", () =>
 </script>
  
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-5 gap-2 ">
+  <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-5  lg:grid-cols-4 xl:grid-cols-5 gap-4">
     <FabricCard
       v-for="fabric in fabrics"
       :key="fabric.name"
