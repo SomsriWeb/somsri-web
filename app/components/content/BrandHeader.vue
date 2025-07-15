a60e2a550fd8e0ab3fbc2d8faeb50681fd01724a
Pawat <38448373+pawataven@users.noreply.github.com>
2025-07-15 13:57:00 +0700

    FEAT: BRAND

---
+++ app/components/content/BrandHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="relative overflow-x-hidden">
+    <header class="relative w-full h-screen overflow-hidden">
+      <!-- Background Image -->
+      <NuxtImg
+        src="/tshirt-factory/bg.png"
+        class="w-full h-full object-cover"
+        alt="Background"
+      />
+
+      <div class="absolute inset-0 bg-black/30"></div>
+
+      <div
+        class="absolute inset-0 grid grid-cols-1 lg:grid-cols-2 items-center justify-center px-4 sm:px-8 lg:px-20 pt-0 pb-0 m-0 text-white"
+      >
+        <!-- ฝั่ง Image -->
+        <div class="order-1 lg:order-2 flex justify-center items-center">
+          <slot mdc-unwrap="p" name="image-header-object" />
+        </div>
+
+        <!-- ฝั่ง Text -->
+        <div class="order-2 lg:order-1 text-center lg:text-left max-w-3xl z-10">
+          <slot name="image-header-logo" />
+
+          <h1 class="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-bold leading-tight">
+            <slot name="title-h1" mdc-unwrap="p" />
+          </h1>
+
+          <h2 class="text-2xl sm:text-3xl font-bold mt-2">
+            <slot name="title-h2" mdc-unwrap="p" />
+          </h2>
+
+          <p class="font-light text-base sm:text-lg lg:text-xl mt-4 mb-5">
+            <slot name="description" mdc-unwrap="p" />
+          </p>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
---
+++ app/components/content/BrandMainContent.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ProseH2 from './ProseH2.vue';
+
+</script>
+
+<template>
+  <div class="flex justify-center text-3xl pb-5 text-center font-bold text-primary">
+    <ProseH2><slot mdc-unwrap="p" name="title-h2" /></ProseH2>
+  </div>
+</template>
