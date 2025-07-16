1802f37341123cc3ad0488c9d9e447c0cf3d02c4
pawataven <pawatwantrong@gmail.com>
2025-07-16 08:37:42 +0700

    ADD: หน้า BAG ครับ

---
+++ app/components/content/BagAskMore.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import ProseH2 from "./ProseH2.vue";
+</script>
+
+<template>
+  <div class="flex justify-center p-4">
+    <div class="bg-primary w-full max-w-2xl sm:max-w-3xl md:max-w-4xl p-6 sm:p-8 rounded-2xl mx-auto">
+      <!-- Title -->
+      <div class="text-center">
+        <ProseH2 class="text-white text-lg">
+          สอบถามเพิ่มเติม
+        </ProseH2>
+      </div>
+      <div class="flex justify-center items-center gap-2 p-4 lg:gap-5 sm:gap-5 md:gap-5">
+        <nuxt-link
+          to="https://www.facebook.com/somsritshirt/"
+          target="_blank"
+          aria-label="Facebook"
+        >
+          <NuxtImg
+            src="/bag/fb-240w.png"
+            class="w-5 sm:w-10 object-contain"
+            alt="Facebook"
+          />
+        </nuxt-link>
+
+        <nuxt-link
+          to="https://contact.somsritshirt.com/3d8f7"
+          target="_blank"
+          aria-label="LINE"
+        >
+          <NuxtImg
+            src="/bag/line-2-unsmushed.png"
+            class="w-5 sm:w-10 object-contain"
+            alt="LINE"
+          />
+        </nuxt-link>
+
+        <nuxt-link
+          to="https://www.instagram.com/somsritshirt/"
+          target="_blank"
+          aria-label="Instagram"
+        >
+          <NuxtImg
+            src="/bag/ig-2-unsmushed.png"
+            class="w-5 sm:w-10 object-contain"
+            alt="Instagram"
+          />
+        </nuxt-link>
+
+        <nuxt-link to="tel:0634216521" target="_blank" aria-label="โทรศัพท์">
+          <NuxtImg
+            src="/bag/tel-1-unsmushed.png"
+            class="w-5 sm:w-10 object-contain"
+            alt="Phone"
+          />
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+ 
+</template>
+
---
+++ app/components/content/BagMainContent.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import ProseH1 from "./ProseH1.vue";
+import ProseH2 from "./ProseH2.vue";
+import ProseP from "./ProseP.vue";
+</script>
+
+<template>
+      <div class="flex justify-center text-center">
+    <ProseH1 class="text-4xl text-primary font-bold"
+      ><slot mdc-unwrap="p" name="title-h1"
+    /></ProseH1>
+  </div>
+
+  <div class="flex justify-center text-center">
+    <ProseH2 class="text-3xl text-primary font-light"
+      ><slot mdc-unwrap="p" name="title-h2"
+    /></ProseH2>
+  </div>
+
+  <div class="flex justify-center text-lg ">
+    <ProseP class="text-primary text-center"
+      ><slot mdc-unwrap="p" name="title-p"
+    /></ProseP>
+  </div>
+
+</template>
+
+<style scoped></style>
