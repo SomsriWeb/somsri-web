58fba45a714d72b7df1e4cba25ee1fe1b8630c15
promproexe <63090004+PromproEXE@users.noreply.github.com>
2025-07-17 10:43:18 +0700

    FEAT: SOFT POWER PAGE

--- app/components/content/ProseH1.vue
+++ app/components/content/ProseH1.vue
@@ -1,3 +1,3 @@
 <template>
-	<h1 class="text-primary text-[4rem] leading-none font-bold"><slot /></h1>
+	<h1 class="text-primary text-5xl lg:text-[4rem] leading-none font-bold"><slot /></h1>
 </template>
