a5844fbffdb29e3cf02c4b9594bd03c3c9d76e9c
promproexe <63090004+PromproEXE@users.noreply.github.com>
2025-07-19 11:56:16 +0700

    FIX: PRODUCT-TYPE

--- app/components/content/ProductDescription.vue
+++ app/components/content/ProductDescription.vue
@@ -1,31 +1,20 @@
-<script setup lang="ts">
-const props = defineProps<{
-  actionLink: string
-}>()
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <!-- ข้อความข้างล่าง -->
-  <div class="px-4 sm:px-8 md:px-12 lg:px-[4.6rem] py-6 md:py-10">
-    <ProseH2
-      class="text-xl sm:text-2xl md:text-3xl xl:text-4xl font-bold mb-4 md:mb-8 text-start text-primary"
-    >
-      เลือกผ้าแบบไหนให้เหมาะกับแบรนด์คุณ
-    </ProseH2>
-    <ProseP class="text-sm sm:text-base md:text-lg text-start">
-      <slot name="products" mdc-unwrap="p" />
-    </ProseP>
-  </div>
+	<div class="space-y-5">
+		<ProseH2
+			class="text-xl sm:text-2xl md:text-3xl xl:text-4xl font-bold text-start text-primary"
+		>
+			เลือกผ้าแบบไหนให้เหมาะกับแบรนด์คุณ
+		</ProseH2>
+		<ProseP class="text-sm sm:text-base md:text-lg text-start">
+			<slot name="products" mdc-unwrap="p" />
+		</ProseP>
 
-  <!-- ปุ่ม -->
-  <div class="flex justify-center my-5">
-    <nuxt-link to="/fabric-specification" class="flex justify-center">
-    <UButton
-      icon="lucide:shirt"
-      class="relative inline-flex rounded-lg px-4 py-2 text-white text-sm sm:text-base md:text-lg bg-primary hover:bg-primary-50 cursor-pointer"
-    >
-      เนื้อผ้าและคุณสมบัติ
-    </UButton>
-    </nuxt-link>
-  </div>
+		<DivCenter>
+			<NuxtLink to="/fabric-specification">
+				<UButton icon="lucide:shirt"> เนื้อผ้าและคุณสมบัติ </UButton>
+			</NuxtLink>
+		</DivCenter>
+	</div>
 </template>
--- app/components/content/ProductHeader.vue
+++ app/components/content/ProductHeader.vue
@@ -1,25 +1,21 @@
 <script setup lang="ts">
-import ProseH1 from "./ProseH1.vue";
-import ProseH2 from "./ProseH2.vue";
-import ProseP from "./ProseP.vue";
+	import ProseH1 from "./ProseH1.vue"
+	import ProseH2 from "./ProseH2.vue"
+	import ProseP from "./ProseP.vue"
 </script>
 
 <template>
-  <div class="px-4 py-6 sm:px-8 md:px-16 lg:px-[4.6rem] lg:py-[3rem]">
-    <ProseH1
-      class=" text-primary text-3xl sm:text-3xl md:text-4xl font-bold text-center p-4 sm:p-5 mb-6 sm:mb-10"
-    >
-      <slot name="title" mdc-unwrap="p" />
-    </ProseH1>
+	<div>
+		<ProseH1 class="text-primary text-3xl sm:text-3xl md:text-4xl font-bold text-center">
+			<slot name="title" mdc-unwrap="p" />
+		</ProseH1>
 
-    <ProseP class="text-base sm:text-lg text-start">
-      <slot name="description" mdc-unwrap="p" />
-    </ProseP>
+		<ProseP class="text-base sm:text-lg text-start mb-5">
+			<slot name="description" />
+		</ProseP>
 
-    <ProseH2
-      class="text-lg sm:text-xl text-center font-bold p-4 sm:p-5 mb-6 sm:mb-10"
-    >
-      <slot name="productheader" mdc-unwrap="p" />
-    </ProseH2>
-  </div>
+		<ProseH2 class="text-lg sm:text-xl text-center font-bold">
+			<slot name="productheader" mdc-unwrap="p" />
+		</ProseH2>
+	</div>
 </template>
