6162e507ce2c8de9a96686861fe0f43d69b447c4
pawataven <pawatwantrong@gmail.com>
2025-07-21 09:53:16 +0700

    FixPort

--- app/components/content/PortCard.vue
+++ app/components/content/PortCard.vue
@@ -14,7 +14,7 @@ const { data: datacards } = await useAsyncData(`portcard-${props.type}`, () => {
 <template>
   <div>
     <div
-      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 place-items-center"
+      class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 place-items-center"
     >
       <div
         v-for="item in datacards"
