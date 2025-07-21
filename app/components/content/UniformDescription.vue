ca5bcd02c3044af9dc98c7e4a9503078ad2699e1
philaphat-2003 <philaphatkaewthane@gmail.com>
2025-07-21 14:15:21 +0700

    แก้ ไข ui

--- app/components/content/UniformDescription.vue
+++ app/components/content/UniformDescription.vue
@@ -6,7 +6,6 @@ const props = defineProps<{
 
 <template>
   <div class="flex flex-col items-center w-full m-0 p-4 space-y-6">
-   
     <!-- หัวเรื่อง -->
     <ProseH1 class="text-center">
       <slot name="title" mdc-unwrap="p" />
@@ -16,20 +15,5 @@ const props = defineProps<{
     <ProseH2 class="text-center text-lg">
       <slot name="description" mdc-unwrap="p" />
     </ProseH2>
-
-    <img
-      src="/polo/step-1024x543.png"
-      alt="uniform"
-      class="w-full max-w-md h-auto object-contain"
-    />
-
-    <!-- ปุ่มสั่งทำเสื้อ -->
-    <div>
-      <a :href="props.actionLink" target="_blank" rel="noopener">
-        <UButton size="xl" color="primary" variant="solid" icon="i-lucide-send">
-          สั่งทำเสื้อ
-        </UButton>
-      </a>
-    </div>
   </div>
 </template>
