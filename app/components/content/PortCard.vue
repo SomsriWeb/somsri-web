975388816cf41e800e139e0a028887418d03438a
Pawat <38448373+pawataven@users.noreply.github.com>
2025-07-21 10:47:31 +0700

    FIX: PORTFOLIO UI AND IMAGE

--- app/components/content/PortCard.vue
+++ app/components/content/PortCard.vue
@@ -14,7 +14,7 @@ const { data: datacards } = await useAsyncData(`portcard-${props.type}`, () => {
 <template>
   <div>
     <div
-      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 place-items-center"
+      class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 place-items-center"
     >
       <div
         v-for="item in datacards"
@@ -31,7 +31,7 @@ const { data: datacards } = await useAsyncData(`portcard-${props.type}`, () => {
         </div>
 
         <!-- Text -->
-        <span class="text-base sm:text-lg font-semibold text-primary mt-3">
+        <span class=" text-base sm:text-lg font-semibold text-primary mt-3">
           {{ item.name }}
         </span>
         <p class="text-sm text-primary mt-1">• {{ item.method }}</p>
