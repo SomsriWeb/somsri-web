6e7170d39762e2d0256fa2994c3e553c80421bdd
philaphat-2003 <philaphatkaewthane@gmail.com>
2025-07-22 12:41:16 +0700

    PAGE: UNIFORM

---
+++ app/components/content/UniformDescription.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+  actionLink: string;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col items-center w-full m-0 p-4 space-y-6">
+    <!-- หัวเรื่อง -->
+    <ProseH1 class="text-center">
+      <slot name="title" mdc-unwrap="p" />
+    </ProseH1>
+
+    <!-- คำอธิบาย -->
+    <ProseH2 class="text-center text-lg">
+      <slot name="description" mdc-unwrap="p" />
+    </ProseH2>
+  </div>
+</template>
