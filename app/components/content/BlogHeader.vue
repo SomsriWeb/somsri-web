46da72743615f35ecad8e2c282df56add10a4ee9
promproexe <63090004+PromproEXE@users.noreply.github.com>
2025-08-05 23:49:51 +0700

    FIX: BLOG URL PREFIX

---
+++ app/components/content/BlogHeader.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+	// PROPS
+	interface Props {
+		image: string
+		titleAsTag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+	}
+	const { image, titleAsTag = "h1" } = defineProps<Props>()
+</script>
+
+<template>
+	<div>
+		<header>
+			<ProseImg :src="image" class="!w-screen !h-screen object-cover" />
+			<div class="absolute inset-0 bg-black/30" />
+			<div class="absolute bottom-[3rem] px-5 md:px-[4.6rem] text-white">
+				<component
+					:is="titleAsTag"
+					class="font-bold text-3xl sm:text-4xl lg:text-6xl leading-none"
+					><slot name="title" mdc-unwrap="p"
+				/></component>
+				<p v-if="$slots['secondary-title']" class="font-bold text-lg lg:text-2xl">
+					<slot name="secondary-title" mdc-unwrap="p" />
+				</p>
+				<p
+					v-if="$slots.description"
+					class="font-light text-lg lg:text-2xl"
+					name="description"
+					mdc-unwrap="p"
+				>
+					<slot name="description" mdc-unwrap="p" />
+				</p>
+
+				<div v-if="$slots.default" class="mt-5">
+					<slot />
+				</div>
+			</div>
+		</header>
+	</div>
+</template>
--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -2,25 +2,43 @@
 	import { LINE_LINK } from "~/lib/lineLink"
 
 	const route = useRoute()
+	const pageType = ref<"page" | "blog">("page")
 
-	const { data: page } = await useAsyncData("page-" + route.path, () => {
-		return queryCollection("content").path(route.path).first()
+	const { data: page } = await useAsyncData("page-" + route.path, async () => {
+		pageType.value = "page"
+		let data = await queryCollection("content").path(route.path).first()
+
+		if (!data) {
+			data = await queryCollection("blog").path(route.path).first()
+			if (data) {
+				pageType.value = "blog"
+			}
+		}
+
+		return data
 	})
 
 	if (!page.value) {
 		throw createError({ statusCode: 404, statusMessage: "Page not found", fatal: true })
 	}
 
-	// console.log(page.value)
-
 	provide(LINE_LINK, page.value.lineLink)
 </script>
 
 <template>
 	<div v-if="page">
-		<NuxtLayout :name="page.activeNavbar ? 'color' : 'default'">
+		<NuxtLayout :name="page.activeNavbar || pageType === 'blog' ? 'color' : 'default'">
 			<Metadata :page="page" />
-			<ContentRenderer class="space-y-[2rem]" :value="page" />
+			<ContentRenderer v-if="pageType === 'page'" class="space-y-[2rem]" :value="page" />
+
+			<template v-else>
+				<BlogHeader :image="page.image">
+					<template #title>{{ page.title }}</template>
+				</BlogHeader>
+				<Container>
+					<ContentRenderer class="space-y-[2rem]" :value="page" />
+				</Container>
+			</template>
 		</NuxtLayout>
 	</div>
 </template>
