17b935b8137374e453fbb947013fdbbbdd508cd3
promproexe <63090004+PromproEXE@users.noreply.github.com>
2025-08-13 00:05:37 +0700

    ADD: PROSE A STYLING

---
+++ app/components/content/ProseA.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+	// PROPS
+	interface Props {
+		href: string
+		target: "_blank" | "_parent" | "_self" | "_top" | (string & object) | null | undefined
+	}
+	const props = defineProps<Props>()
+</script>
+<template>
+	<NuxtLink :href="props.href" :target="props.target" class="underline text-primary-500">
+		<slot />
+	</NuxtLink>
+</template>
