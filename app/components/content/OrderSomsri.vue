e17aae3af211ccbcde129eca6295508c8969dcc2
promproexe <63090004+PromproEXE@users.noreply.github.com>
2025-09-18 21:58:40 +0700

    REFACTOR: ORDER SOMSRI PROPS

--- app/components/content/OrderSomsri.vue
+++ app/components/content/OrderSomsri.vue
@@ -1,26 +1,20 @@
 <script setup lang="ts">
-// PROPS
-interface Props {
-    titleTag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
-    descriptionTag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
-}
-const { titleTag = 'p', descriptionTag = 'p' } = defineProps<Props>();
+import { LINE_LINK } from '~/lib/lineLink';
+
+// VARIABLE
+const lineLink = inject(LINE_LINK, 'https://line.me/R/ti/p/%40diz8986o');
 </script>
 <template>
     <div>
         <div class="rounded-2xl p-3 md:p-7 bg-primary">
             <div class="text-center text-3xl lg:text-5xl font-bold text-white mb-3 leading-none">
-                <component :is="titleTag">
-                    <slot name="title" mdc-unwrap="p" />
-                </component>
+                <slot name="title" mdc-unwrap="h1 h2 h3 h4 h5 h6 p" />
             </div>
             <p class="text-center text-white text-base md:text-lg font-light mb-5">
-                <component :is="descriptionTag">
-                    <slot name="description" mdc-unwrap="p" />
-                </component>
+                <slot name="description" mdc-unwrap="h1 h2 h3 h4 h5 h6 p" />
             </p>
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-3 md:gap-5">
-                <NuxtLink href="https://contact.somsritshirt.com/3d975" external>
+                <NuxtLink :href="lineLink" external>
                     <UButton icon="hugeicons:line" color="neutral" class="!text-primary px-2 w-full">@somsritshirt</UButton>
                 </NuxtLink>
 
