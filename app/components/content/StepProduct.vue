245257905acc43dbe36788a79035a84276b65da9
promproexe <63090004+PromproEXE@users.noreply.github.com>
2025-09-26 12:44:19 +0700

    ADD: STEP PRODUCT STORIES

--- app/components/content/StepProduct.vue
+++ app/components/content/StepProduct.vue
@@ -1,8 +1,7 @@
-<script setup></script>
 <template>
-	<section class="bg-primary pt-36">
-		<div class="flex justify-center items-center">
-			<ProseImg src="/how-to-order/step_produce.png" alt="ขั้นตอนการผลิต" />
-		</div>
-	</section>
+    <section class="bg-primary pt-36">
+        <div class="flex justify-center items-center">
+            <ProseImg src="/how-to-order/step_produce.png" alt="ขั้นตอนการผลิต" />
+        </div>
+    </section>
 </template>
