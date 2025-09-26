df9ade8ad5075d011bbf804b09ed0ca4a37c2cb1
promproexe <63090004+PromproEXE@users.noreply.github.com>
2025-09-26 12:59:20 +0700

    ADD: TSHIRT FACTORY IMAGE

---
+++ app/components/content/TshirtFactoryImage.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import TshirtFactoryImage from './TshirtFactoryImage.vue';
+
+// More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
+
+const meta = {
+    title: 'Components/TshirtFactoryImage',
+    component: TshirtFactoryImage,
+    // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
+    tags: ['autodocs'],
+} satisfies Meta<typeof TshirtFactoryImage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const TshirtFactoryImageStory: Story = {
+    args: {},
+};
