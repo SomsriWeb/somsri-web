8f5c7728c7fadd5edbb317d267d34848fee2d7ee
promproexe <63090004+PromproEXE@users.noreply.github.com>
2025-09-26 12:46:00 +0700

    ADD: TITLE WITH DESCRIPTION STORIES

--- app/components/content/TitleWithDescription.vue
+++ app/components/content/TitleWithDescription.vue
@@ -1,6 +1,21 @@
+<script setup lang="ts">
+// SLOTS
+interface Slots {
+    /**
+     * หัวข้อ (ใส่ Component อะไรก็ได้)
+     */
+    title(): unknown;
+
+    /**
+     * รายละเอียด (ใส่ Component อะไรก็ได้)
+     */
+    description(): unknown;
+}
+defineSlots<Slots>();
+</script>
 <template>
-	<div>
-		<slot name="title" />
-		<slot name="description" />
-	</div>
+    <div>
+        <slot name="title" />
+        <slot name="description" />
+    </div>
 </template>
