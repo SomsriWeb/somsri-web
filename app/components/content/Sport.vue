6cc2e463663457493c815636be3182d8638a35c8
promproexe <63090004+PromproEXE@users.noreply.github.com>
2025-09-26 12:35:44 +0700

    ADD: SPORT STORIES

--- app/components/content/Sport.vue
+++ app/components/content/Sport.vue
@@ -1,24 +1,41 @@
 <script setup lang="ts">
-import ProseH1 from './ProseH1.vue';
-import ProseH2 from './ProseH2.vue';
-import ProseP from './ProseP.vue';
+// SLOTS
+interface Slots {
+    /**
+     * ภาพที่แสดงผล (ใส่เป็น Component ภาพ)
+     */
+    image(): unknown;
 
+    /**
+     * หัวข้อ (แสดงเป็น H1)
+     */
+    title(): unknown;
+
+    /**
+     * ข้อความรอง (แสดงเป็น H2)
+     */
+    subtitle(): unknown;
+
+    /**
+     * รายละเอียด (แสดงเป็น P)
+     */
+    description(): unknown;
+}
+defineSlots<Slots>();
 </script>
 
 <template>
     <!--รูปภาพ เนื้อหา-->
     <div class="flex justify-center pb-5">
-      <slot mdc-unwrap="p" name="image" />
-  </div>
+        <slot mdc-unwrap="p" name="image" />
+    </div>
 
-  <!--ข้อความหัวข้อ-->
-  <div class="text-center text-primary text-3xl font-bold pb-5 m-0">
-    <ProseH1><slot name="title-text" mdc-unwrap="p" /></ProseH1>
-    <ProseH2><slot name="sub-title-text" mdc-unwrap="p" /></ProseH2>
-    <ProseP class="text-lg font-light">
-      <slot mdc-unwrap="p" name="discription-text" />
-    </ProseP>
-
-
-  </div>
+    <!--ข้อความหัวข้อ-->
+    <div class="text-center text-primary text-3xl font-bold pb-5 m-0">
+        <ProseH1><slot name="title" mdc-unwrap="p" /></ProseH1>
+        <ProseH2><slot name="subtitle" mdc-unwrap="p" /></ProseH2>
+        <ProseP class="text-lg font-light">
+            <slot mdc-unwrap="p" name="description" />
+        </ProseP>
+    </div>
 </template>
