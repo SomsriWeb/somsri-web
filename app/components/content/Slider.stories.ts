cb133c86b66f1567ec23f5c540ffb3c660cb5e4e
promproexe <63090004+PromproEXE@users.noreply.github.com>
2025-09-26 12:28:52 +0700

    ADD: SLIDER STORIES

--- app/components/content/Slider.stories.ts
+++ app/components/content/Slider.stories.ts
@@ -1,18 +1,20 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
-import Size from './Size.vue';
+import Slider from './Slider.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
 
 const meta = {
-    title: 'Components/Size',
-    component: Size,
+    title: 'Components/Slider',
+    component: Slider,
     // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
     tags: ['autodocs'],
-} satisfies Meta<typeof Size>;
+} satisfies Meta<typeof Slider>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const SizeStory: Story = {
-    args: {},
+export const SliderStory: Story = {
+    args: {
+        items: 1,
+    },
 };
