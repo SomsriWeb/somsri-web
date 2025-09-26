40905cd0e50c07e8bf9ef23faefad5714a7a2bf1
philaphat-2003 <philaphatkaewthane@gmail.com>
2025-09-26 11:29:15 +0700

    ADD:bloglist

---
+++ app/components/content/BlogList.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import BlogList from './BlogList.vue';
+
+// More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
+
+const meta = {
+    title: 'Components/BlogList',
+    component: BlogList,
+    // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
+    tags: ['autodocs'],
+} satisfies Meta<typeof BlogList>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const BlogListStory: Story = {
+    args: {
+        blog: {},
+        
+    },
+};
