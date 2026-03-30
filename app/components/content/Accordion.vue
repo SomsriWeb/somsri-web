1f50e19ea5092cc3d9487f5b7ca6bd43f083f689
nerdchild6 <posorun.posorungg@gmail.com>
2026-03-30 16:20:39 +0700

    REFACTOR: align with other component

--- app/components/content/Accordion.vue
+++ app/components/content/Accordion.vue
@@ -47,13 +47,13 @@ function toggleAccordion(index: number) {
 </script>
 
 <template>
-    <div class="max-w-2xl mx-auto px-4">
-        <h2 class="text-xl font-semibold text-[#a52241] mb-2">คำถามที่ลูกค้าชอบถาม</h2>
+    <div class="px-4 lg:relative lg:-left-4 lg:min-w-[105%]">
+        <h2 class="text-4xl font-semibold text-primary mb-2">คำถามที่ลูกค้าชอบถาม</h2>
 
         <div class="space-y-4">
             <div class="rounded-lg bg-white border border-3 border-[#e5e5e5]">
                 <div class="flex items-center p-3 text-lg font-semibold cursor-pointer rounded-lg select-none" @click="toggleAccordion(0)">
-                    <span class="mr-3 text-2xl font-bold text-[#a52241]">{{ isOpen[0] ? '−' : '+' }}</span>
+                    <span class="mr-3 text-2xl font-bold text-primary">{{ isOpen[0] ? '−' : '+' }}</span>
                     ซักแล้วลายจะหลุดไหม
                 </div>
                 <div class="text-gray-600 p-4" v-show="isOpen[0]">ไม่หลุด ไม่แตก ไม่ลอก เพราะใช้การพิมพ์แบบ Sublimation สีจะซึมเข้าไปในเนื้อผ้า ทำให้ลายติดทน ซักได้ยาวๆ สีไม่ซีดง่าย</div>
@@ -61,7 +61,7 @@ function toggleAccordion(index: number) {
 
             <div class="rounded-lg bg-white border border-3 border-[#e5e5e5]">
                 <div class="flex items-center p-3 text-lg font-semibold cursor-pointer rounded-lg select-none" @click="toggleAccordion(1)">
-                    <span class="mr-3 text-2xl font-bold text-[#a52241]">{{ isOpen[1] ? '−' : '+' }}</span>
+                    <span class="mr-3 text-2xl font-bold text-primary">{{ isOpen[1] ? '−' : '+' }}</span>
                     ทำขั้นต่ำกี่ตัว?
                 </div>
                 <div class="text-gray-600 p-4" v-show="isOpen[1]">เริ่มต้นขั้นต่ำ 100 ตัวเท่านั้น</div>
@@ -69,7 +69,7 @@ function toggleAccordion(index: number) {
 
             <div class="rounded-lg bg-white border border-3 border-[#e5e5e5]">
                 <div class="flex items-center p-3 text-lg font-semibold cursor-pointer rounded-lg select-none" @click="toggleAccordion(2)">
-                    <span class="mr-3 text-2xl font-bold text-[#a52241]">{{ isOpen[2] ? '−' : '+' }}</span>
+                    <span class="mr-3 text-2xl font-bold text-primary">{{ isOpen[2] ? '−' : '+' }}</span>
                     ใช้เวลาผลิตกี่วัน?
                 </div>
                 <div class="text-gray-600 p-4" v-show="isOpen[2]">รับประกันได้รับสินค้าภายใน 90 วัน</div>
@@ -77,7 +77,7 @@ function toggleAccordion(index: number) {
 
             <div class="rounded-lg bg-white border border-3 border-[#e5e5e5]">
                 <div class="flex items-center p-3 text-lg font-semibold cursor-pointer rounded-lg select-none" @click="toggleAccordion(3)">
-                    <span class="mr-3 text-2xl font-bold text-[#a52241]">{{ isOpen[3] ? '−' : '+' }}</span>
+                    <span class="mr-3 text-2xl font-bold text-primary">{{ isOpen[3] ? '−' : '+' }}</span>
                     เสื้อจะหดหรือย้วยไหม?
                 </div>
                 <div class="text-gray-600 p-4" v-show="isOpen[3]">ไม่หด ไม่ย้วยง่าย เพราะเป็นผ้าโพลีเอสเตอร์ อยู่ทรงดี ซักแล้วไม่เสียรูป</div>
