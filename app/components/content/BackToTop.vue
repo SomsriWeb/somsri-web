4707cdbbeca5e204af70ce78aa6ada654e401322
nerdchild6 <posorun.posorungg@gmail.com>
2026-03-31 09:55:42 +0700

    REFACTOR: finish v1

---
+++ app/components/content/BackToTop.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue';
+
+const showButton = ref(false);
+
+const handleScroll = () => {
+    // Show button if user has scrolled down more than 200px
+    showButton.value = window.scrollY > 200;
+};
+
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth', // Smooth scrolling animation
+    });
+};
+
+onMounted(() => {
+    window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => {
+    // Clean up event listener when component is unmounted
+    window.removeEventListener('scroll', handleScroll);
+});
+</script>
+
+<template>
+    <UButton v-if="showButton" icon="i-lucide-arrow-up" class="absolute fixed bottom-4 right-4 rounded-full !min-w-0" @click="scrollToTop" aria-label="Back to top" />
+</template>
