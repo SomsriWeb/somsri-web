8a1349fc20302cc22287073435d409aa1f2961fe
adison <std.65122420119@ubru.ac.th>
2026-04-29 10:16:07 +0700

    Update ProductGrid Layout and Add New Embroidery Types
    - Adjusted the grid layout in ProductGrid.vue for better responsiveness on larger screens.
    - Added new JSON files for various embroidery types including Appliqué, Arm, Patch, Original, and Transfer, enhancing the product offerings.
    - Updated existing JSON files to correct image paths and orders for Arm embroidery types.
    - Removed outdated JSON files related to "Our Favorite" and "Satin" categories to streamline the data structure.

--- app/components/content/ProductGrid.vue
+++ app/components/content/ProductGrid.vue
@@ -23,7 +23,7 @@ const { data: products } = await useAsyncData('products', () => queryCollection(
 <template>
     <section>
         <ProseH2 class="text-5xl mb-5">สินค้าของเรา</ProseH2>
-        <div class="grid grid-cols-2 md:grid-cols-4 gap-5">
+        <div class="grid grid-cols-2 lg:grid-cols-4 gap-5">
             <NuxtLink v-for="(item, index) in products" :key="index" :to="item.url">
                 <Card class="shadow hover:scale-105 transition duration-300">
                     <ProseImg :src="item.image" :alt="item.alt" class="aspect-square object-cover rounded-lg" />
