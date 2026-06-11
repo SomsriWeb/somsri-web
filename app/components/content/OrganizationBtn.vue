56c72e412f3041ef9fbb65137e577234d985f20e
Adison Weluwanarak <151486254+adison119@users.noreply.github.com>
2026-06-11 13:43:54 +0700

    Fix polo page (#195)
    * Update stats-banner counts in index.md
    
    * Add 5 Articles
    
    Co-authored-by: Nuxt Studio <noreply@nuxt.studio>
    
    * Add Image
    
    Co-authored-by: Nuxt Studio <noreply@nuxt.studio>
    
    * Add 1 Articles
    
    Co-authored-by: Nuxt Studio <noreply@nuxt.studio>
    
    * Add 4 Articles
    
    Co-authored-by: Nuxt Studio <noreply@nuxt.studio>
    
    * Edit Polo Page
    
    Co-authored-by: Nuxt Studio <noreply@nuxt.studio>
    
    * Add 3 Articles
    
    Co-authored-by: Nuxt Studio <noreply@nuxt.studio>
    
    * Add 4 Article
    
    Co-authored-by: Nuxt Studio <noreply@nuxt.studio>
    
    * Enhance AvailableFabric and Carousel Components; Add Fabric Comparison
    
    * portfolio-page : create new portfolio-artist, portfolio-organization page , New button link to portfolio-artist, portfolio-organization
    
    * fix-component-somsriproducefor : separate the logo images, one per brand. select only the logos of well-known brands to add separate alt tags for each brand.
    
    * fix line contact
    
    * fix according to reviews
    
    ---------
    
    Co-authored-by: adison <std.65122420119@ubru.ac.th>
    Co-authored-by: SomsriWeb <web.somsritshirt@outlook.com>
    Co-authored-by: Nuxt Studio <noreply@nuxt.studio>
    Co-authored-by: Keerati <Keerati.guy.c@gmail.com>

---
+++ app/components/content/OrganizationBtn.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-center relative mb-10">
+    <NuxtLink to="/portfolio-organization" class="block">
+      <UButton
+        size="xl"
+        color="primary"
+        variant="solid"
+        class="relative"
+      >
+        ดูผลงานกลุ่มลูกค้าองค์กร
+      </UButton>
+    </NuxtLink>
+  </div>
+</template>
